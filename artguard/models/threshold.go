587c3a4e915366792083dba0d0236f3eecb6b3d4
Viktor Novomlynskyi <viktor.novomlynskyi@nure.ua>
2025-06-19 22:48:35 +0300

    Delete artguard directory

--- artguard/models/threshold.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Threshold struct {
-	ID         int     `json:"id"`
-	ZoneID     int     `json:"zone_id"`
-	SensorType string  `json:"sensor_type"`
-	MinValue   float64 `json:"min_value"`
-	MaxValue   float64 `json:"max_value"`
-}
